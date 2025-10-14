18ab5cd0607f744518111a1f085da6969c0d295d
5677 <cincoseissietesiete@gmail.com>
2025-10-14 14:45:28 -0300

    Home mejorado

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, make_response
+import os 
+import json
 
 app = Flask(__name__)
+VOTES_FILE = 'encuestas.json'
 
 @app.route('/')
-def Home():
-    return render_template('Home.html')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
